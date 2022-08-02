c5ec4d7aa10b2886c8c033c7d2b1dfffb9fdd9af
anreh <anurehka@gmail.com>
2022-08-02 10:58:24 +0800

    das

--- app/src/main/java/com/example/constrainlayout/MainActivity.kt
+++ app/src/main/java/com/example/constrainlayout/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import org.w3c.dom.Text
 
@@ -15,6 +16,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setListeners() {
+
+
+        val redButton = findViewById<Button>(R.id.red_button)
+        val greenButton = findViewById<Button>(R.id.green_button)
+        val yellowButton = findViewById<Button>(R.id.yellow_button)
         val boxOneText = findViewById<TextView>(R.id.boxOne)
         val boxTwoText = findViewById<TextView>(R.id.boxTwo)
         val boxThreeText = findViewById<TextView>(R.id.boxThree)
@@ -22,9 +28,12 @@ class MainActivity : AppCompatActivity() {
         val boxFiveText = findViewById<TextView>(R.id.boxFive)
 
         val clickableViews: List<View> =
-            listOf(
-                boxOneText, boxTwoText, boxThreeText, boxFourText, boxFiveText
+            listOf(boxOneText, boxTwoText, boxThreeText,
+                boxFourText, boxFiveText,
+                redButton, greenButton, yellowButton
             )
+
+        )
 
         for (item in clickableViews) {
             item.setOnClickListener { makeColoured(it) }
@@ -38,8 +47,13 @@ class MainActivity : AppCompatActivity() {
             R.id.boxThree -> view.setBackgroundColor(Color.RED)
             R.id.boxFour -> view.setBackgroundColor(Color.YELLOW)
             R.id.boxFive -> view.setBackgroundColor(Color.MAGENTA)
+
+            R.id.red_button -> boxThree.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
+
 }
 
